2d88ece0a495d0229a5bbf8532c4257e979cabbe
Bubu <73459333+bububirb@users.noreply.github.com>
2025-04-09 17:51:18 +0800

    Refactor: Modular Event Handling
    Splits events handling into multiple files in the event directory.

--- index.js
+++ index.js
@@ -28,33 +28,19 @@ for (const folder of commandFolders) {
 	}
 }
 
-// Ready function
-client.once(Events.ClientReady, readyClient => {
-	console.log(`${readyClient.user.tag} is logged in`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Log in to Discord
 client.login(token);
 
-// Interaction listener
-client.on(Events.InteractionCreate, async interaction => {
-	if (!(interaction.isChatInputCommand() || interaction.isContextMenuCommand())) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-		}
-	}
-});
